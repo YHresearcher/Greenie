f0aca375c25ee7cd7f451da91d412258e9b8d287
YHresearcher <p.nhatthanh87@gmail.com>
2026-05-04 23:21:21 +0700

    ok

--- astro-blog/astro.config.mjs
+++ astro-blog/astro.config.mjs
@@ -1,7 +1,5 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
 
-export default defineConfig({
-  site: 'https://greenievietnam.live',
-  base: '/',
-  output: 'static',
-});
+// https://astro.build/config
+export default defineConfig({});
